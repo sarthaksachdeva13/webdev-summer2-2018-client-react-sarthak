cbe303ae6424f9c544fffd7a09e0763cdb4bfba8
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-27 17:55:33 -0400

    Added more actions and constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,53 +1,139 @@
 import * as constants from '../constants/index';
 
-export const headingNameChanged = (dispatch, widgetId, newName) => (
+export const findAllWidgetsForTopic = (dispatch,topicId) => {
+    fetch('http://localhost:8080/api/topic/'+ topicId + "/widget")
+        .then(response => (response.json()))
+        .then(widgets => dispatch({
+            type: constants.FIND_ALL_WIDGETS_FOR_TOPIC,
+            widgets: widgets }))
+};
+
+// Heading Actions
+export const changeHeadingName = (dispatch, widgetId, headingName) => (
     dispatch({
-        type: constants.HEADING_NAME_CHANGED,
+        type: constants.CHANGE_HEADING_NAME,
         id: widgetId,
-        name: newName
+        name: headingName
     })
 );
 
-export const headingTextChanged = (dispatch, widgetId, newText) => (
+export const changeHeadingText = (dispatch, widgetId, headingText) => (
     dispatch({
-        type: constants.HEADING_TEXT_CHANGED,
+        type: constants.CHANGE_HEADING_TEXT,
         id: widgetId,
-        text: newText
+        text: headingText
     })
 );
-export const headingSizeChanged = (dispatch, widgetId, newSize) => (
+export const changeHeadingSize = (dispatch, widgetId, headingSize) => (
     dispatch({
-        type: constants.HEADING_SIZE_CHANGED,
+        type: constants.CHANGE_HEADING_SIZE,
         id: widgetId,
-        size: newSize
-    })
-);
-
-
-export const listNameChanged = (dispatch, widgetId, newListName) => (
-    dispatch({
-        type: constants.LIST_NAME_CHANGED,
-        id: widgetId,
-        name: newListName
+        size: headingSize
     })
 );
 
-export const listTextChanged = (dispatch,widgetId,newListText) =>(
+
+// List Actions
+export const changeListName = (dispatch, widgetId, listName) => (
+    dispatch({
+        type: constants.CHANGE_LIST_NAME,
+        id: widgetId,
+        name: listName
+    })
+);
+
+export const changeListText = (dispatch,widgetId,listText) =>(
 
     dispatch({
-        type : constants.LIST_TEXT_CHANGED,
+        type : constants.CHANGE_LIST_TEXT,
         id: widgetId,
-        text: newListText
+        text: listText
 
     })
 
 );
 
-export const listTypeChanged = (dispatch,widgetId,listType) =>(
+export const changeListType = (dispatch,widgetId,listType) =>(
     dispatch({
-        type : constants.LIST_TYPE_CHANGED,
+        type : constants.CHANGE_LIST_TYPE,
         id: widgetId,
         listType: listType
     })
 );
 
+
+
+// Paragraph Actions
+export const changeParagraphText = (dispatch,widgetId, paraText) =>(
+    dispatch({
+        type : constants.CHANGE_PARA_TEXT,
+        id: widgetId,
+        text: paraText
+    })
+);
+
+export const changeParagraphName = (dispatch,widgetId, paraName) =>(
+    dispatch({
+        type : constants.CHANGE_PARA_NAME,
+        id: widgetId,
+        name: paraName
+    })
+);
+
+
+
+// Image Actions
+export const changeImageText = (dispatch,widgetId,imageText) =>(
+    dispatch({
+        type : constants.CHANGE_IMAGE_TEXT,
+        id: widgetId,
+        text: imageText
+    })
+);
+
+export const changeImageName = (dispatch,widgetId,imageName) =>(
+    dispatch({
+        type : constants.CHANGE_IMAGE_NAME,
+        id: widgetId,
+        name: imageName
+    })
+);
+
+
+//Link Actions
+export const changeLinkText = (dispatch,widgetId,linkText) =>(
+    dispatch({
+        type : constants.CHANGE_LINK_TEXT,
+        id: widgetId,
+        text: linkText
+    })
+);
+
+export const changeLinkDisplay = (dispatch,widgetId,linkDisplay) =>(
+    dispatch({
+        type : constants.CHANGE_LINK_DISPLAY,
+        id: widgetId,
+        linkName: linkDisplay
+    })
+);
+
+export const changeLinkName = (dispatch,widgetId,linkName) =>(
+    dispatch({
+        type : constants.CHANGE_LINK_NAME,
+        id: widgetId,
+        name: linkName
+    })
+);
+
+
+// Misc constants
+export const addWidget = dispatch => (
+    dispatch({type: constants.ADD_WIDGET})
+);
+export const save = (dispatch,topicId) => (
+    dispatch({type: constants.SAVE,
+        topicId : topicId})
+);
+export const preview = dispatch => (
+    dispatch({type: constants.PREVIEW})
+);
