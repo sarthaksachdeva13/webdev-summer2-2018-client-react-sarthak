523a3860f586970d52360bf1c3c293f83baf6b3b
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-28 17:51:56 -0400

    Finished widget component

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {DELETE_WIDGET} from "../constants/index"
 import * as actions from '../actions'
-import * as widgetConstant from '../constants'
-import {changeListName} from "../actions";
-import {changeListText} from "../actions";
-import {changeListType} from "../actions";
+import * as widgetConstant from '../constants';
+
+let widgetContainerStyle =
+    {
+        border: "solid", borderWidth: "thin", width: "1000px", borderColor: "gray", borderRadius: "3px"
+    };
 
 
 //Heading Widget
@@ -411,3 +413,211 @@ const stateToPropertyMapperList = state => (
 
 
 const ListContainer = connect(stateToPropertyMapperList, dispatchToPropertyMapperList)(List);
+
+
+//Link Widget
+
+const Link = ({widget, preview, changeLinkText, changeLinkName, changeLinkDisplay}) => {
+
+    let linkInput, linkName, linkDisplay;
+    return (
+        <html>
+        <head>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+                  integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+                  crossOrigin="anonymous"/>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+                    integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+                  integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+                  crossOrigin="anonymous"/>
+        </head>
+        <body>
+        <div className="container widget-container"
+             style={widgetContainerStyle}>
+            <div className="col-md-12">
+                <div hidden={preview}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <input onChange={() => changeLinkText(widget.id, linkInput.value)}
+                                       ref={node3 => linkInput = node3}
+                                       value={widget.text} className="form-control"
+                                /> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <input onChange={() => changeLinkDisplay(widget.id, linkDisplay.value)}
+                                       ref={node3 => linkDisplay = node3}
+                                       value={widget.linkName} className="form-control"
+                                /> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+
+                                <input onChange={() => changeLinkName(widget.id, linkName.value)}
+                                       value={widget.name}
+                                       ref={node => linkName = node} className="form-control"/> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <h4>Link Preview</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+                            <a href={widget.text}>{widget.linkName}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
+
+
+    )
+};
+
+const dispatchToPropertyMapperLink = dispatch =>
+    ({
+        changeLinkText:
+            (widgetId, linkText) =>
+                actions.changeLinkText(dispatch, widgetId, linkText),
+        changeLinkDisplay:
+            (widgetId, linkDispText) =>
+                actions.changeLinkDisplay(dispatch, widgetId, linkDispText),
+        changeLinkName:
+            (widgetId, linkName) =>
+                actions.changeLinkName(dispatch, widgetId, linkName)
+    });
+
+const stateToPropertyMapperLink = state => ({
+    preview: state.preview
+});
+
+const LinkContainer = connect(stateToPropertyMapperLink, dispatchToPropertyMapperLink)(Link);
+
+
+let widgetListStyle =
+    {
+        height: "37px", borderRadius: "3px"
+    };
+
+const Widget = ({widget, preview, dispatch, widgetLength}) => {
+    let selectElement;
+    return (
+        <html>
+        <head>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+                  integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+                  crossOrigin="anonymous"/>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+                    integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+                  integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+                  crossOrigin="anonymous"/>
+        </head>
+        <body>
+        <div hidden={preview}>
+            <div className="container widget-container"
+                 style={widgetContainerStyle}>
+                <div className="row">
+                    <div className="col-md-12 text-dark pt-2">
+                        <div className="row flex-row pb-1">
+                            <div className="col-md-3 d-inline-flex">
+                                <h4>{widget.widgetType}</h4></div>
+                            <div className="col-md-9">
+
+                                <div className="d-inline-flex pr-2 float-right">
+                                    <button onClick={e => (
+                                        dispatch({type: DELETE_WIDGET, id: widget.id})
+                                    )} className="btn btn-danger"><i className="fas fa-times"/></button>
+                                </div>
+
+
+                                <div className="d-inline-flex pr-2 float-right">
+                                    <button onClick={e => (
+                                        dispatch({
+                                            type: widgetConstant.INCREASE_ORDER_WIDGET,
+                                            widgetOrder: widget.widgetOrder
+                                        })
+                                    )} disabled={(widget.widgetOrder === widgetLength)} className="btn btn-warning"><i
+                                        className="fas fa-arrow-down"/></button>
+                                </div>
+
+
+                                <div className="d-inline-flex pr-2 float-right">
+                                    <button onClick={e => (
+                                        dispatch({
+                                            type: widgetConstant.DECREASE_ORDER_WIDGET,
+                                            widgetOrder: widget.widgetOrder
+                                        })
+                                    )} disabled={(widget.widgetOrder === 1)} className="btn btn-warning"><i
+                                        className="fas fa-arrow-up"/></button>
+                                </div>
+                                <div className="d-inline-flex pr-1 float-right my-auto" style={widgetListStyle}>
+                                    <select value={widget.widgetType}
+                                            onChange={e =>
+                                                dispatch({
+                                                    type: 'SELECT_WIDGET_TYPE',
+                                                    id: widget.id,
+                                                    widgetType: selectElement.value
+                                                })} ref={node => selectElement = node}>
+                                        <option>Heading</option>
+                                        <option>Paragraph</option>
+                                        <option>List</option>
+                                        <option>Image</option>
+                                        <option>Link</option>
+                                    </select>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            {widget.widgetType === 'Heading' && <HeadingContainer widget={widget}/>}
+            {widget.widgetType === 'Paragraph' && <ParaContainer widget={widget}/>}
+            {widget.widgetType === 'List' && <ListContainer widget={widget}/>}
+            {widget.widgetType === 'Image' && <ImageContainer widget={widget}/>}
+            {widget.widgetType === 'Link' && <LinkContainer widget={widget}/>}
+        </div>
+
+        </body>
+        </html>
+    )
+};
+
+const WidgetContainer = connect(state => (
+    {
+        preview: state.preview
+    }
+))(Widget);
+
+export default WidgetContainer;
+
+
+
