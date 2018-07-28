30ff23a0eb2e2308cbc6f68134e88d0929408c87
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-28 16:03:59 -0400

    Added Paragrapht and Image in Widget Component

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {DELETE_WIDGET} from "../constants/index"
 import * as actions from '../actions'
-import * as widgetConstant from '../constants';
+import * as widgetConstant from '../constants'
 
+
+//Heading Widget
 const Heading = ({widget, preview, changeHeadingText, changeHeadingSize, changeHeadingName}) => {
 
     let headingSize, headingInput, headingName;
@@ -85,7 +87,7 @@ const Heading = ({widget, preview, changeHeadingText, changeHeadingSize, changeH
 };
 
 
-const dispatchToPropertyMapper = dispatch => ({
+const dispatchToPropertyMapperHeading = dispatch => ({
     changeHeadingText:
         (widgetId, newText) =>
             actions.changeHeadingText(dispatch, widgetId, newText),
@@ -97,11 +99,186 @@ const dispatchToPropertyMapper = dispatch => ({
             actions.changeHeadingSize(dispatch, widgetId, newSize)
 });
 
-const stateToPropsMapper = state => (
+const stateToPropertyMapperHeading = state => (
     {
         preview: state.preview
     }
 );
 
 
-const HeadingContainer = connect(stateToPropsMapper, dispatchToPropertyMapper)(Heading);
+const HeadingContainer = connect(stateToPropertyMapperHeading, dispatchToPropertyMapperHeading)(Heading);
+
+
+//Paragraph Widget
+
+const Paragraph = ({widget, preview, changeParagraphText, changeParagraphName}) => {
+    let paraInput, paraName;
+    return (
+        <html>
+        <head>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+                  integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+                  crossOrigin="anonymous"/>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+                    integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+                  integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+                  crossOrigin="anonymous"/>
+        </head>
+        <body>
+        <div className="container widget-container"
+             style={widgetContainerStyle}>
+            <div className="col-md-12">
+                <div hidden={preview}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+        <textarea onChange={() => changeParagraphText(widget.id, paraInput.value)}
+                  ref={node => paraInput = node}
+                  value={widget.text} className="form-control">
+    </textarea> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <input onChange={() => changeParagraphName(widget.id, paraName.value)}
+                                       value={widget.name}
+                                       ref={node => paraName = node} className="form-control"/> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <h4>Paragraph Preview</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+                            <p> {widget.text} </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
+    )
+};
+
+const dispatchToPropertyMapperParagraph = dispatch =>
+    ({
+        changeParagraphText:
+            (widgetId, newText) =>
+                actions.changeParagraphText(dispatch, widgetId, newText),
+        changeParagraphName:
+            (widgetId, newName) =>
+                actions.changeParagraphName(dispatch, widgetId, newName)
+    });
+
+const stateToPropertyMapperParagraph = state => (
+    {
+        preview: state.preview
+    }
+);
+
+const ParaContainer = connect(stateToPropertyMapperParagraph, dispatchToPropertyMapperParagraph)(Paragraph)
+
+
+//Image Widget
+
+const Image = ({widget, preview, changeImageText, changeImageName}) => {
+    let imageInput, imageName;
+    return (
+        <html>
+        <head>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+                  integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+                  crossOrigin="anonymous"/>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+                    integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+                    crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+                  integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+                  crossOrigin="anonymous"/>
+        </head>
+        <body>
+        <div className="container widget-container"
+             style={widgetContainerStyle}>
+            <div className="col-md-12">
+                <div hidden={preview}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <input onChange={() => changeImageText(widget.id, imageInput.value)}
+                                       ref={node3 => imageInput = node3}
+                                       value={widget.text} className="form-control"
+                                /> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <input onChange={() => changeImageName(widget.id, imageName.value)}
+                                       value={widget.name}
+                                       ref={node => imageName = node} className="form-control"/> <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+
+                                <h4>Image Preview</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+                            <div className="col-md-2">
+                                <img src={widget.text} alt={widget.text}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
+
+    )
+};
+
+
+const dispatchToPropertyMapperImage = dispatch =>
+    ({
+        changeImageText:
+            (widgetId, imageText) =>
+                actions.changeImageText(dispatch, widgetId, imageText),
+        changeImageName:
+            (widgetId, imageName) =>
+                actions.changeImageName(dispatch, widgetId, imageName)
+    });
+
+const stateToPropertyMapperImage = state => (
+    {
+        preview: state.preview
+    }
+);
+
+const ImageContainer = connect(stateToPropertyMapperImage, dispatchToPropertyMapperImage)(Image);
